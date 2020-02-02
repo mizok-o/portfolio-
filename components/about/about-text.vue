2b0964dcdfcd1b91742c3a3f1c5b21d2fda15f65
mizok-o <mizoko320@gmail.com>
2020-02-02 13:28:03 +0900

    aboutページ訂正

---
+++ components/about/about-text.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  p.about-metext Currently I can write css and html and a little bit vue.js. I’m working as an intern in web studio. I’m not interested in only web, I like architecture, graphic, ui, product as well. I’d like to try many new things. I like coffee, mangas, films, cheeseburger and travel.
+    | I post on instagram about my travel. My dream is remote-working abroad with a  computer once.
+</template>
+<style lang="sass">
+.about-metext
+  margin: 16% 0 0
+</style>
